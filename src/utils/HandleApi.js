0e54a9d9abf8e416fba4ebea5a075706cd6c070a
swathiravi-vh <swathiit01@gmail.com>
2023-04-24 20:15:31 +0530

    updated url

--- src/utils/HandleApi.js
+++ src/utils/HandleApi.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "http://localhost:5000";
+const baseUrl = "https://todoapp-backend-91e5.onrender.com";
 
 const getAllToDo = (setToDo) => {
   axios.get(baseUrl).then(({ data }) => {
